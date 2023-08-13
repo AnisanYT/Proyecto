fc4bd69b556d51bbf9ea96203e3560eefa82bcd3
AnisanYT <mejiadani07@gmail.com>
2023-08-12 21:12:37 -0600

    Proyecto, ew, old

---
+++ Proyecto nuevo/JS/index.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  let valor = document.getElementById("miParrafo");
+  valor.textContent =
+    'Costa Rica se encuentra en el "Anillo de Fuego del Pacífico", lo que significa que está salpicada de volcanes y actividad geotermal. Esto ha dado lugar a la creación de aguas termales naturales que fluyen desde las profundidades de la Tierra. Muchos destinos, como La Fortuna (cerca del Volcán Arenal) y Rincón de la Vieja, cuentan con complejos de aguas termales que ofrecen una variedad de piscinas y pozas con diferentes temperaturas, lo que te permite sumergirte en el lujo cálido de la madre naturaleza';
+  let title = document.getElementById("titleParrafo");
+  title.textContent = "Relajación en Aguas Termales y Spas Naturales";
+  title.style.padding = "4px";
+}
